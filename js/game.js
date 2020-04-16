d44b77e9fdc86526d355c00bc24f708274f5edde
viettran <viet.tranhoang@powergatesoftware.com>
2020-04-16 13:54:57 +0700

    done logic game

--- js/game.js
+++ js/game.js
@@ -30,13 +30,32 @@ var group = [
     ['./TemplateData/img/red1.png', './TemplateData/img/red2.png'],
     ['./TemplateData/img/red1.png', './TemplateData/img/red3.png'],
 ];
-var listObjShowup = [];
+var listObjShowup = [], time = 10, pickedCards = [], successCount = 0;
 var shuffle = function (a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
     }
     return a;
+}
+var startGame = function () {
+    $('.card').addClass('flip-active');
+    setTimeout(() => {
+        closeAllCard();
+        countDownTime();
+    }, 2000);
+}
+var closeAllCard = function () {
+    $('.card').removeClass('flip-active');
+}
+var countDownTime = function () {
+    var interVal = setInterval(function () {
+        time--;
+        $('#time').html(time)
+        if (time <= 0) {
+            clearInterval(interVal);
+        }
+    }, 1000)
 }
 var init = function () {
     listObjShowup = shuffle(listObj).slice(0, 12);
@@ -45,7 +64,37 @@ var init = function () {
         var frontId = '#front' + (i + 1);
         $(frontId).attr("src", listObjShowup[i]);
     }
+    /* init logic */
+    startGame();
+}
+var pair = function (pickedCards) {
+    group.forEach(function (e) {
+        if (e.includes(pickedCards[0]) && e.includes(pickedCards[1])) {
+            console.log('paired', true);
+            successCount++;
+            $('#successCount').text(successCount);
+        }
+    })
 }
 $(function () {
     init();
+    $('.card').click(function () {
+        if (pickedCards.length >= 2 || $(this).hasClass("flip-active")) {
+            return;
+        } else {
+            var index = $(this).attr('index');
+            var frontUrl = $('#front' + index).attr('src');
+            pickedCards.push(frontUrl);
+            $(this).addClass('flip-active');
+
+        }
+        /*pair*/
+        if (pickedCards.length === 2) {
+            pair(pickedCards);
+            setTimeout(() => {
+                closeAllCard();
+                pickedCards = [];
+            }, 1000);
+        }
+    })
 });
